5226e8f1d3d59c648580204998c182276550fa09
Julian <Julian.K.Siow@gmail.com>
2020-07-23 13:44:32 +0200

    adds some payment data to be displayed

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 $('form').on('submit', (e) => {
     e.preventDefault();
 
+    $('.payment-data').empty();
+    $('.receipt-container').empty();
+    $('.payment-data').append('<div class="spinner-border text-primary" role="status"> <span class="sr-only">Loading...</span> </div>')
+
     const plateNum = $('#licensePlateNum').val();
     const date = $('#date').val();
     const cardNum = $('#cardNum').val();
@@ -8,29 +12,23 @@ $('form').on('submit', (e) => {
     const queriedDay = new Date(date);
     const day = new Date(queriedDay).getTime()/1000;
     const followingDay = new Date(queriedDay.getTime() + 86400000)/1000;
-    
-
-    console.log(queriedDay/1000);
-    console.log(followingDay)
 
     $.ajax({
         url: `http://localhost:3000/api/v1/payments/${plateNum}/${day}/${followingDay}/${cardNum}`,
         type: 'GET',
         success: (res) => {
+            $('.payment-data').empty();
+            $('.receipt-container').empty();
             if (res.data.length < 1) {
                 $('.payment-container').append('<h3 class="err">No matching reciept was found... :(</h3>')
             } else {
                 res.data.forEach(payment => {
-                    $('.payment-container').append(`<pre class="receipt">${payment.receipt}</pre>`)
+                    console.log(payment)
+                    $('.payment-data').append(`<p>Payment Amount: ${payment.payment_payload.amount}`)
+                    $('.payment-data').append(`<p>Payment Type: ${payment.payment_payload.payment_type}`)
+                    $('.receipt-container').append(`<pre>${payment.receipt}</pre>`)
                 });
             }
         }
     });
 })
-
-const dbDate = new Date(1531839805135.056).setHours(0,0,0,0);
-
-// const dbTimestamp = dbDate.getTime();
-
-console.log('db date: ' + dbDate);
-// console.log('db timestamp: ' + dbTimestamp);
